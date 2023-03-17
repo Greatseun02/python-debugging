01b1a38d2a2ce08aeb815712b478d20586cca0c0
Goodnews Adewole <goodnews.adewole@example.com>
2023-03-17 11:46:45 +0100

    debugged

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -5,7 +5,7 @@
 import sys
 
 
-def area_of_rectangle(height, width = None):
+def area_of_rectangle(height:int, width = None):
     """
     Returns the area of a rectangle.
 
@@ -29,9 +29,11 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
-        width = height
-    area = height * width
+    height = int(height)
+    
+    width = int(width)
+    
+    area = width * height
     return area
 
 if __name__ == '__main__':
@@ -42,10 +44,10 @@ if __name__ == '__main__':
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
     height = sys.argv[1]
-    width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
-
+    if len(sys.argv) > 2:
+        width = sys.argv[2]
+    else:
+        width = height
     area = area_of_rectangle(height, width)
 
     message = "The area of a {h} X {w} rectangle is {a}".format(
